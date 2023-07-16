59fc70e8407af0a386cd3ff37a5e48f342916616
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-07-16 16:28:38 +0000

    Create the list view for all tasks

--- types.d.ts
+++ types.d.ts
@@ -9,3 +9,30 @@ interface NavLinks {
   icon: IconType;
   href: string;
 }
+
+interface Task {
+  id: string;
+  title: string;
+  priority: string;
+  status: string;
+  start_date: string;
+  end_date: string;
+}
+
+interface Project {
+  id: string;
+  title: string;
+  status: string;
+  start_date: string;
+  end_date: string;
+  tasks: Task[];
+}
+
+interface ProjectsViewProps {
+  projects: Project[];
+  totalProjects: number;
+  rows: number;
+  first: number;
+  setFirst: (first: number) => void;
+  setRows: (rows: number) => void;
+}
